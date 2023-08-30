73d8a75c48f883dd141a5fd92f8d87684bc886ab
lenarddome <lenarddome@gmail.com>
2023-08-30 15:34:27 +0100

    update demonstration

--- demonstration.py
+++ demonstration.py
@@ -9,6 +9,11 @@ output = np.zeros((data.shape[0], data.shape[1], data.shape[1]), dtype = int)
 # create all inequality matrices 
 for i in range(data.shape[0]):
     probabilities = np.asarray(data[i, :])
-    output[i] = cl.imac(probabilities.squeeze(), thresholds = [0.1, -0.1])
+    output[i] = cl.imac(probabilities.squeeze(), thresholds = [0.33, -0.33])
 
 print(output)
+len(output)
+
+unimac, indices, counts = np.unique(output, axis = 0, return_inverse=True, return_counts=True)
+print(unimac)
+print(counts)
